40bcd6dc067917616b316c848549c05cfe6d7387
miroma21 <miromax21@gmail.com>
2019-03-26 19:24:38 +0400

    Initial Commit

---
+++ htc_learn_1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  htc_learn_1
+//
+//  Created by Princess Max on 26.03.2019.
+//  Copyright © 2019 Princess Max. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
